1e6aba219f206a361cabead65457ef299c3d43ea
Daniel <danleavitt0@gmail.com>
2016-08-04 17:46:55 -0700

    font size

--- src/components/noTeams.js
+++ src/components/noTeams.js
@@ -10,7 +10,10 @@ function render ({props}) {
     <Card tall wide>
       <Flex p='40px' tall wide column align='start center'>
         <Text block fs='40px'> Waiting For Teams To Join </Text>
-        <Text block fs='25px'> Game ID: <Text color='red'>{id}</Text> </Text>
+        <Flex block fs='25px' column align='center center'>
+          <Text>Game ID:</Text>
+          <Text mt='40px' fs='125px' color='red'>{id}</Text>
+        </Flex>
       </Flex>
     </Card>
   )
